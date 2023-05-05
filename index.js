a3b6b39ceb74cbec944cd307506cdb0f67ac82c1
mailorcanto <mailorcanto@gmail.com>
2023-05-05 17:00:43 -0300

    upload

---
+++ index.js
@@ -0,0 +1,61 @@
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("eu sou lindo")
+// }
+
+// const resposta = outraFuncao("eu sou lindo")
+ // console.log(resposta)
+
+//  console.log("meu nome é joão, tenho 15 anos e sou de são leopoldo")
+
+// let nome = prompt("qual seu nome?")
+// let idade = Number(prompt("qual sua idade?"))
+// let cidade = prompt ("qual sua cidade?")
+// let profissao = prompt("qual sua profissão?")
+
+// function listaPessoa(nome,idade,cidade,profissao){
+// 	console.log(`eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`) 
+// }
+ 
+// listaPessoa(nome,idade,cidade,profissao)
+
+//  function somar(numero1, numero2){
+//  	return numero1 + numero2
+// }
+//  console.log(somar(80,20))
+
+// function verificarNumeroMaior(numero1,numero2){
+// 	return numero1 >= numero2
+// }
+// console.log(somar(80,20))
+// console.log(verificarNumeroMaior(80,20))
+
+
+// function calculadora(n1,n2){
+// 	const soma = n1 + n2
+// 	const dif = n1 - n2
+// 	const mult = n1 * n2
+// 	const divisao = n1 / n2
+// 	return [soma,dif,mult,divisao]
+// }
+// const numero1 = Number(prompt("digite um numero"))
+// const numero2 = Number(prompt("digite outro numero"))
+
+// console.log(calculadora(numero1,numero2))
+
+// const imprimir = (parametro) =>{
+// 	console.log(parametro)
+// }
+// const somatoria = (n1,n2) =>{
+// 	const soma = n1 = n2
+// 	imprimir(soma)
+// }
+// somatoria(10,30)
